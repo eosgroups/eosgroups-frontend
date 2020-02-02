67bd2f42a7ddc2e6078f3e59cf615ce2c9c33d80
unknown <kasperkasperfish@gmail.com>
2020-02-02 20:43:22 +0100

    add mini logo

--- src/components/main-logo.vue
+++ src/components/main-logo.vue
@@ -2,7 +2,7 @@
   <div class="q-mt-xs relative-position">
     <router-link to="/" >
       <img src="~assets/daclify-logo.svg" style="height:40px" class="cursor-pointer gt-xs" />
-      <img src="~assets/daclify-logo.svg" style="height:10px" class="cursor-pointer lt-sm" />
+      <img src="~assets/daclify-d-transparent.svg" style="height:40px" class="cursor-pointer lt-sm" />
     </router-link>
     <!-- <span class="bg-primary rounded-borders inline q-ml-xs" >BETA</span> -->
   </div>
