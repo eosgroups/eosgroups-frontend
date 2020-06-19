9a98ca8e8c789e3b225db6db2cbe8a72cd76f576
unknown <piecesnbits@protonmail.com>
2020-06-19 14:24:24 +0200

    sync

--- src/components/external-component.vue
+++ src/components/external-component.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <q-btn label="test" color="primary"></q-btn>
-    {{src}}
+    <q-btn label="test" color="primary" @click="fetchBalance()"></q-btn>
+    
   </div>
 </template>
 
@@ -10,17 +10,39 @@
 module.exports = {
   name: 'externalComponent',
   props:{
-    src:{
+    token:{
+      type: Object,
+      default: ()=>{return {contract:"eosio.token",symbol:"EOS"}}
+    },
+    account:{
       type: String,
-      default: "hello"
+      default: ""
     }
   },
   data () {
     return {
-
+      res: "no"
     }
   },
   methods:{
+    async fetchBalance(){
+      let res = await this.$eos.rpc.get_table_rows({
+        json: true,
+        code: this.token.contract,
+        scope: this.account,
+        table: "accounts",
+        lower_bound: this.account,
+        upper_bound: this.account,
+        limit: 1
+      }).catch(e => false);
+      if(res){
+        this.res = res
+      }
+      else{
+        return [];
+      }
+      
+    }
 
   },
   async mounted(){
